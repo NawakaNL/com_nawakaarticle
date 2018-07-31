8f94d6c7f51ecbc277ccb50b5a1844e20f105a40
Tristan de Boer <info@tristandeboer.nl>
2018-07-31 09:53:55 +0200

    Initial Commit

---
+++ site/nawakaarticle.php
@@ -0,0 +1,156 @@
+<?php
+
+// Initialize Database
+$db = JFactory::getDbo();
+
+$url = "https://deliver.kenticocloud.com/bf245c4e-805c-0006-f04d-81be5711929e/items";
+
+// Get API contents
+$ch = curl_init();
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
+$result= json_decode(curl_exec($ch), true)["items"];
+curl_close($ch);
+
+foreach($result as $item) {
+  $article_id = $item["system"]["id"];
+  $title = $item["elements"]["artikeltitel"]["value"];
+  $lead = $item["elements"]["lead"]["value"];
+  $text = $item["elements"]["tekst"]["value"];
+  $youtube = $item["elements"]["youtube_embed_url"]["value"];
+  $embed = $item["elements"]["embed_url"]["value"];
+
+  echo $embed;
+
+  $photographers = array();
+  foreach($item["elements"]["welke_fotograaf_heeft_de_foto_s_gemaakt_"]["value"] as $photographer){
+    array_push($photographers, $photographer["name"]);
+  }
+  $photographers = implode(", ", $photographers);
+
+
+  $authors = array();
+  foreach($item["elements"]["wie_is_zijn_de_auteurs_van_dit_stukje_"]["value"] as $author){
+    array_push($authors, $author["name"]);
+  }
+  $authors = implode(", ", $authors);
+
+  // Check if a lead photo has been specified and if it exists
+  if (!empty($item["elements"]["lead_foto"]["value"][0])
+  && !file_exists(JPATH_BASE.'/images/articles/'.$item["elements"]["lead_foto"]["value"][0]["name"])) {
+    // Lead photo does not exist, upload a new one.
+    file_put_contents(JPATH_BASE.'/images/articles/'.$item["elements"]["lead_foto"]["value"][0]["name"],
+      fopen($item["elements"]["lead_foto"]["value"][0]["url"], 'r'));
+  }
+
+  // Check if other photos have been specified and if they exist.
+  $photos = array();
+  foreach ($item["elements"]["foto_s"]["value"] as $photo) {
+    if (!empty($photo)) {
+      array_push($photos, $photo["name"]);
+      if (!file_exists(JPATH_BASE.'/images/articles/'.$photo["name"])) {
+        // Lead photo does not exist, upload a new one.
+        file_put_contents(JPATH_BASE.'/images/articles/'.$photo["name"],
+          fopen($photo["url"], 'r'));
+      }
+    }
+  }
+
+  $lead_url = "";
+  if (!empty($item["elements"]["lead_foto"]["value"][0])) {
+    $lead_url = JUri::base().'images/articles/'.$item["elements"]["lead_foto"]["value"][0]["name"];
+  }
+  $full_lead_url = $lead_url;
+
+  if (!empty($embed)) {
+    $full_lead_url = $embed;
+  }
+
+  if (!empty($youtube)) {
+    $full_lead_url = $youtube;
+  }
+
+  $extra_html = "";
+  if ($photos) {
+    $extra_html .= '<link href="/components/com_nawakaarticle/flickity.min.css" rel="stylesheet" type="text/css" media="screen" />';
+    $extra_html .= '<script src="/components/com_nawakaarticle/flickity.pkgd.min.js"></script>';
+    $extra_html .= '<div class="flickity" data-flickity=\'{ "imagesLoaded": true, "percentPosition": false }\'>';
+    foreach ($photos as $photo) {
+      $extra_html .= '<img src="'.JUri::base().'images/articles/'.$photo.'" />';
+    }
+    $extra_html .= '</div>';
+  }
+
+  // Check if article already exists
+  $query = $db->getQuery(true);
+  $query->select($db->quoteName(array('id')));
+  $query->from($db->quotename('#__content'));
+  $query->where($db->quoteName('metadata'). ' LIKE '.$db->quote('%'.$article_id.'%'));
+  $db->setQuery($query);
+  $results = $db->loadObjectList();
+
+  if (!empty($results) && count($results) > 0) {
+    // Article already exist, update article
+
+    if (version_compare(JVERSION, '3.0', 'lt')) {
+        JTable::addIncludePath(JPATH_PLATFORM . 'joomla/database/table');
+    }
+    $article = & JTable::getInstance('content');
+    $id = $results[0]->id;
+    $article->load(array('id' => $results[0]->id));
+    $article->title            = $title;
+    $article->introtext        = $lead;
+    $article->fulltext         = $text . $extra_html;
+    $article->created_by_alias = $authors;
+    $article->images          = '{"image_intro":"'.$lead_url.'","float_intro":"","image_intro_alt":"","image_intro_caption":"","image_fulltext":"'.$full_lead_url.'", "float_fulltext":"none","image_fulltext_alt":"","image_fulltext_caption":""}';
+    $article->access           = 1;
+    $article->metadata         = '{"page_title":"","rights":"'.$photographers.'", "author":"'.$authors.'","robots":"", "xreference": "'.$article_id.'"}';
+    $article->language         = '*';
+
+    // Check to make sure our data is valid, raise notice if it's not.
+
+    // Check the data.
+    if (!$article->check())
+    {
+
+    }
+
+    // Store the data.
+    if (!$article->store())
+    {
+
+    }
+  } else {
+    // Create new Article
+    if (version_compare(JVERSION, '3.0', 'lt')) {
+        JTable::addIncludePath(JPATH_PLATFORM . 'joomla/database/table');
+    }
+    $article = JTable::getInstance('content');
+    $article->title            = $title;
+    $article->alias            = JFilterOutput::stringURLSafe($title);
+    $article->introtext        = $lead;
+    $article->fulltext         = $text . $extra_html;
+    $article->catid            = 9;
+    $article->created          = JFactory::getDate()->toSQL();
+    $article->created_by_alias = $authors;
+    $article->images          = '{"image_intro":"'.$lead_url.'","float_intro":"","image_intro_alt":"","image_intro_caption":"","image_fulltext":"'.$full_lead_url.'", "float_fulltext":"none","image_fulltext_alt":"","image_fulltext_caption":""}';
+    $article->state            = 0;
+    $article->access           = 1;
+    $article->metadata         = '{"page_title":"","rights":"'.$photographers.'", "author":"'.$authors.'","robots":"", "xreference": "'.$article_id.'"}';
+    $article->language         = '*';
+
+    // Check to make sure our data is valid, raise notice if it's not.
+
+    if (!$article->check()) {
+    }
+
+    // Now store the article, raise notice if it doesn't get stored.
+
+    if (!$article->store(TRUE)) {
+    }
+  }
+}
+
+?>
